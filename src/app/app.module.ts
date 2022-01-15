a8bf358c904136fdc2678ae81c49a418ff551c1a
tarashrytsiv <hrt.taras@gmail.com>
2022-01-15 18:03:03 +0200

    feature - added bootstrap to the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AlertModule } from 'ngx-bootstrap/alert';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AlertModule
   ],
   providers: [],
   bootstrap: [AppComponent]
